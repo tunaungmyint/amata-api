53622697d44f3dd32fdf05df83ac66a0e21cab39
Tun Aung Myint <kakyi99999@gmail.com>
2022-12-05 19:43:07 +0630

    fuck

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
-
+app.use(express.static('./public'));
 app.use('/api/users', userRoutes);
 
 const connectDB = async () => {
